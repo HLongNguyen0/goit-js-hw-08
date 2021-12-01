5f46c1962afb6e23feaa81b90c02acdc52a53246
H.Long Nguyen <84514305+HLongNguyen0@users.noreply.github.com>
2021-12-01 19:38:00 +0200

    v2.3.3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,17 +5,20 @@ const form = document.querySelector('.feedback-form');
 
 let userMsg = JSON.parse(localStorage.getItem(key));
 
-form.addEventListener('input', throttle(saveUserMsg, 500));
-form.addEventListener('submit', submitUserMsg);
-
-if (localStorage.getItem(key)) {
+if (userMsg) {
   for (const elem in userMsg) {
     form.elements[elem].value = userMsg[elem];
   }
 }
 
-const userInfo = {};
+form.addEventListener('input', throttle(saveUserMsg, 500));
+form.addEventListener('submit', submitUserMsg);
+
 function saveUserMsg(event) {
+  let userInfo = {};
+  if (localStorage.getItem(key)) {
+    userInfo = JSON.parse(localStorage.getItem(key));
+  }
   userInfo[event.target.name] = event.target.value;
   localStorage.setItem(key, JSON.stringify(userInfo));
 }
